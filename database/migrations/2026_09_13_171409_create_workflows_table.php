e9738b09282fafe186046425cff74549ec3d4e84
Shafny Hadhy <mohamedshafny02@gmail.com>
2026-09-14 10:05:02 +0530

    workflow migration with fields

---
+++ database/migrations/2026_09_13_171409_create_workflows_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('workflows', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('workspace_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->string('name', 150);
+            $table->string('slug', 150);
+            $table->text('description')->nullable();
+
+            $table->timestamps();
+
+            $table->unique(['workspace_id', 'slug']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('workflows');
+    }
+};
