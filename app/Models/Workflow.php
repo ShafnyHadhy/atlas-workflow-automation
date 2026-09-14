9f6a397c8fa60177e14039eb066b156fbe365a93
Shafny Hadhy <mohamedshafny02@gmail.com>
2026-09-14 10:03:26 +0530

    workflow model created with fillables and relationships

---
+++ app/Models/Workflow.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Workflow extends Model
+{
+    /** @use HasFactory<\Database\Factories\WorkflowFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'workspace_id',
+        'name',
+        'slug',
+        'description'
+    ];
+
+    public function workspace(): BelongsTo
+    {
+        return $this->belongsTo(Workspace::class);
+    }
+
+    // public function versions(): HasMany
+    // {
+    //     return $this->hasMany(WorkflowVersion::class);
+    // }
+}
