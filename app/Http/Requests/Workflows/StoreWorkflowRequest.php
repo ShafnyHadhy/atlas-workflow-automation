d7e5211d5d20f7d2461acb62d7ba268576fa66c6
Shafny Hadhy <mohamedshafny02@gmail.com>
2026-09-16 09:36:00 +0530

    validation layer for storing  workflow

---
+++ app/Http/Requests/Workflows/StoreWorkflowRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests\Workflows;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreWorkflowRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:150'],
+            'description' => ['nullable', 'string'],
+        ];
+    }
+}
