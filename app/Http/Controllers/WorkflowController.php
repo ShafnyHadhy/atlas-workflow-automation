de051a2fae9561b8325a1bb84f0e9b4e282675d3
Shafny Hadhy <mohamedshafny02@gmail.com>
2026-09-20 21:41:27 +0530

    update workflow method implemented with calling draft version in action

--- app/Http/Controllers/WorkflowController.php
+++ app/Http/Controllers/WorkflowController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Actions\Workflows\CreateWorkflow;
+use App\Actions\Workflows\UpdateWorkflowDraft;
 use App\Http\Requests\Workflows\StoreWorkflowRequest;
+use App\Http\Requests\Workflows\UpdateWorkflowDraftRequest;
 use App\Http\Resources\WorkflowResource;
 use App\Models\Workflow;
+use App\Models\WorkflowVersion;
 use App\Models\Workspace;
 use Illuminate\Http\RedirectResponse;
 use Inertia\Inertia;
@@ -45,5 +48,22 @@ class WorkflowController extends Controller
             'workspace' => $workspace,
             'workflow' => (new WorkflowResource($workflow))->resolve(),
         ]);
+    }
+
+    public function update(
+        UpdateWorkflowDraftRequest $request,
+        Workspace $workspace,
+        Workflow $workflow,
+        WorkflowVersion $version,
+        UpdateWorkflowDraft $updateWorkflowDraft,
+    ): RedirectResponse {
+
+        $updateWorkflowDraft->execute(
+            version: $version,
+            nodes: $request->validated('nodes'),
+            edges: $request->validated('edges')
+        );
+
+        return redirect()->back();
     }
 }
